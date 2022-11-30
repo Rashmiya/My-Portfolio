b32277df13150861490f750c1ec4c23429a5961d
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-11-30 15:58:10 +0530

    add background image moving

--- assignments/JS/Game/assets/js/logic.js
+++ assignments/JS/Game/assets/js/logic.js
@@ -18,19 +18,46 @@ function startAnimationIdle(){
     idleAnimationNumber = setInterval(idleAnimation,100);
 }
 
-/*when press Enter key character should run*/
+/*when press ArrowRight key, character should run*/
 function keyEvent(event){
-    if(event.key=='Enter'){
+    if (event.key == 'Tab' || event.key == "Enter"){
+        event.preventDefault();
+    }
+    if(event.key=='ArrowRight'){
         clearInterval(idleAnimationNumber);
-        setInterval(runAnimation,100);
-        /*console.log("Enter pressed");*/
+        idleImage=-1;
+        startAnimationRun();    /*start the run Animation*/
+        console.log("ArrowRight pressed");
+
+        if(moveBackgroundAnimationId == 0){
+            moveBackgroundAnimationId = setInterval(moveBackgroundImage,100);
+        }
+    }
+    if(event.key=="ArrowUp"){
+
     }
 }
+runAnimationNumber=0;
+runImage=-1;
+
+/*character run state*/
 function runAnimation(){
-    idleImage = idleImage+1;
-    if(idleImage==10){
-        idleImage=0;
+    runImage = runImage+1;
+    if(runImage==10){
+        runImage=0;
     }
-    $("#sprite").attr("src","assets/images/sprite/Run__00"+idleImage+".png");
-    console.log("Run__00"+idleImage+".png");
+    $("#sprite").attr("src","assets/images/sprite/Run__00"+runImage+".png");
+    /*console.log("Run__00"+runImage+".png");*/
+}
+function startAnimationRun(){
+    runAnimationNumber = setInterval(runAnimation,100);
+}
+
+/*moving background Image*/
+var backgroundImagePositionX = 0;
+moveBackgroundAnimationId =0;
+
+function moveBackgroundImage(){
+    backgroundImagePositionX = backgroundImagePositionX-20;
+    document.getElementById("gamePage").style.backgroundPositionX = backgroundImagePositionX+"px";
 }
