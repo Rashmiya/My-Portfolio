2fbde4a8c503edde184ee9def977a71af75af374
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-11-30 12:32:16 +0530

    add character run state

--- assignments/JS/Game/assets/js/logic.js
+++ assignments/JS/Game/assets/js/logic.js
@@ -17,3 +17,20 @@ idleAnimationNumber=0;
 function startAnimationIdle(){
     idleAnimationNumber = setInterval(idleAnimation,100);
 }
+
+/*when press Enter key character should run*/
+function keyEvent(event){
+    if(event.key=='Enter'){
+        clearInterval(idleAnimationNumber);
+        setInterval(runAnimation,100);
+        /*console.log("Enter pressed");*/
+    }
+}
+function runAnimation(){
+    idleImage = idleImage+1;
+    if(idleImage==10){
+        idleImage=0;
+    }
+    $("#sprite").attr("src","assets/images/sprite/Run__00"+idleImage+".png");
+    console.log("Run__00"+idleImage+".png");
+}
